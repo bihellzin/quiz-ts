c5f96b01866fcc551afccf071e7a061fd6993552
bihellzin <gof2@cin.ufpe.br>
2020-07-14 20:57:24 -0300

    image removed

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,7 +1,5 @@
 import styled, { createGlobalStyle } from 'styled-components'
 
-import BGImage from './images/pic.jpg'
-
 export const GlobalStyle = createGlobalStyle `
   html {
     height: 100%;
